2b41ed8a4f39e8077ddfbde971e95360066776c6
hillaryhusband <hillaryhusband@gmail.com>
2020-11-10 20:16:05 -0600

    New update with source to original paper

--- pbpkme_examplescript.R
+++ pbpkme_examplescript.R
@@ -1,15 +1,26 @@
 rm(list=ls())
 
+
+source("hillaryhusband/Documents/GitHub/pbpkme")
 ## run this chunk once to install package needed to pull 'pbpkme' from Github
 install.packages("devtools")
 library(devtools)
 install_github("hillaryhusband/pbpkme")
 ############
-library(pbpkme)
+library("pbpkme")
+library(pracma)
+library(readxl)
+library(lattice)
+library(ggplot2)
+library(tidyverse)
+library(ggthemes)
 
-working.directory <- ("C:/Users/HH/Dropbox/Research - Hillary")
-data1 <- read_excel(file.path(working.directory, "DataIndex.xlsx"), "3cptex2")
-data1 <- as.data.frame(data1[,1:2])
+dt = 0.01
+tfinal = 60
+
+### Parameters and model based on Remifentanil model by Cascone et al.:
+##  Cascone S, Lamberti G, Titomanlio G, Piazza O. Pharmacokinetics of Remifentanil:
+## a three-compartmental modeling approach. Translational Medicine. 2013;7(4):18–22
 
 #parameters
 k10 <- 0.172    # min^-1
@@ -28,24 +39,35 @@ CL3 <- 0.0784 # mL/min
 
 body_weight <- 79 # kg (mean value)
 
-#package parameters into one term
-parameters <- data.frame(k10 = 0.172, k12 = 0.373, k13 = 0.0367, k21 = 0.103, k31 = 0.0124, V1 = 7.88, V2 = 23.9, V3 = 13.8, CL1 = 2.08, CL2 = 0.828, CL3 = 0.0784)
+p_matrix = matrix(0, nrow=3, ncol=3)
+p_matrix[1,1] = -((CL1/V1) + k12 + k13 + k10)
+p_matrix[1,2] = (k21 * V2) / V1
+p_matrix[1,3] = (k31 * V3) / V1
+p_matrix[2,1] = (k12 * V1) / V2
+p_matrix[2,2] = -((CL2/V2) + k21)
+p_matrix[3,1] = (k13 * V1) / V3
+p_matrix[3,3] = -((CL3/V3) + k31)
 
-# example describes 3 compartment model
-# dCp/dt = - CL1*C1 + k21*V2*C2 + k31*V3*C3 - (k12+k13+k10)*C1)*V1 + dose) / V1
-# dC2/dt = (k12 *C1*V1 - k21*C2*V2 - CL2*C2) / V2
-# dC3/dt = (k13*C1*V1 - k31*C3*V3 - CL3*C3) / V3
 
-# as matrix
-p_matrix <- matrix(0, nrow = 3, ncol = 3)
+params <- c(k10 = 0.172, k12 = 0.373, k13 = 0.0367, k21 = 0.103, k31 = 0.0124, V1 = 7.88,
+            V2 = 23.9, V3 = 13.8, CL1 = 2.08, CL2 = 0.828, CL3 = 0.0784)
+# drug injection
+# R_in = 5 * body_weight
 
- # p_matrix[1,1] = -((k10 + k12 + k13)*V1 + CL1) / V1
- # p_matrix[1,2] = (k21*V2) / V1
- # p_matrix[1,3] = (k31*V3) / V1
- # p_matrix[2,1] = (k12*V1) / V2
- # p_matrix[2,2] = - (k21*V2 + CL2) / V2
- # p_matrix[3,1] = (k13*V1) / V3
- # p_matrix[3,3] = - (k31*V3 + CL3) / V3
+# C1 = 0
+# C2 = 0
+# C3 = 0
+
+init_cond_matrix = matrix(0, nrow = 3, ncol = 1)
+
+init_cond_matrix[1,1] = 0 #C1
+init_cond_matrix[2,1] = 0 #C2
+init_cond_matrix[3,1] = 0 #C3
+
+#dC1_dt = (((-CL1*C1 + k21*V2*C2 + k31*V3*C3 - (k12+k13+k10)*C1)*V1) + R_in) / V1
+#dC2_dt = (k12 *C1*V1 - k21*C2*V2 - CL2*C2) / V2
+#dC3_dt = (k13*C1*V1 - k31*C3*V3 - CL3*C3) / V3
+
 
 p_matrix = matrix(0, nrow=3, ncol=3)
 p_matrix[1,1] = -((CL1/V1) + k12 + k13 + k10)
@@ -56,77 +78,30 @@ p_matrix[2,2] = -((CL2/V2) + k21)
 p_matrix[3,1] = (k13 * V1) / V3
 p_matrix[3,3] = -((CL3/V3) + k31)
 
-p_matrix
 
-# define dose
-begin <- 0   # min
-end <- 0.5     # min
+begin <- 0
+end <- 0.5
 dose_number <- 1
-dose_amt <- 30 *body_weight / V1 # 1 * body_weight / V1 #micrograms per kg
-
-# initial conditions - amount of drug in compartment at time 0
-initial_condition_x <- matrix(0, nrow = 3, ncol = 1)
-initial_condition_x[1,1] = 0 #Cp
-initial_condition_x[2,1] = 0 #C2
-initial_condition_x[3,1] = 0 #C3
-
-initial_condition_x
-#
-#
-# pred_time <- c(1.10080446,
-#                2.0084653157,
-#                2.9121645872,
-#                3.972341131,
-#                5.0325176749,
-#                6.0921990207,
-#                7.0186774013,
-#                8.0793491432,
-#                9.9340390974,
-#                11.9224272149,
-#                14.9742106634,
-#                19.8858836453,
-#                25.0634673598,
-#                29.9803399209,
-#                39.8138374443,
-#                59.8863837631)
-# pred_conc <- c(70.9108628188,
-#                19.7896272624,
-#                4.4337123319,
-#                3.660451177,
-#                3.0220505563,
-#                2.4274174178,
-#                1.9230709498,
-#                1.6318745996,
-#                1.1275187146,
-#                0.8118522503,
-#                0.5769069087,
-#                0.3779003773,
-#                0.2475758807,
-#                0.2163643909,
-#                0.1629965866,
-#                0.1340392725)
-#
-# pred_data <- data.frame(pred_time, pred_conc)
+dose_amt <- 30
 
 
-tic()
-model <- pbpkme(100, 0.05, p_matrix, initial_condition_x, parameters, 1, dose_amt, begin, end)
-toc()
+model1 <- pbpkme(minute = tfinal, h = dt, rate_coeff = p_matrix, init_condition = init_cond_matrix,
+                 pars = params, number_of_doses = dose_number, dose = dose_amt, start_time = begin,
+                 stop_time = end)
+
+time <- seq(from=0, to = 60, by = 0.1)
+conc1 <- model1[1,]
+conc2 <- model1[2,]
+conc3 <- model1[3,]
+
+C1 <- data.frame(time, conc1)
+C2 <- data.frame(time, conc2)
+C3 <- data.frame(time, conc3)
 
 
-Cp <- model[1,]
-pred_time <- seq(0,100, by = 0.05)
-pred <- cbind(pred_time,Cp)
-pred<-as.data.frame(pred)
-obs_time <- as.matrix(data1$Minute)
-obs_conc <- as.matrix(data1$microMolar)
-obs <- cbind(obs_time,obs_conc)
-obs <- as.data.frame(obs)
-
-pl1 <- ggplot(data = obs, aes(x=V1, y=V2, colour = "Observed Concentration")) + geom_point(size = 2) +
-   scale_y_log10() + ggtitle("Conc vs Time Curve of Remifentanil", subtitle = "30 microgram/kg bolus") +
-   theme_classic(base_size = 14) + xlab("Time (minutes)") + ylab("Concentration (microMolar)") +
-   theme(axis.text=element_text(size=12), axis.title=element_text(size=14)) +
-   theme(legend.title = element_blank(), legend.position = c(0.7,0.8)) + annotation_logticks(sides = "l")
-pl2 <- pl1 + geom_line(data = pred, aes(x=pred_time, y=Cp, colour = "Model Predictions"), size = 0.8)
-pl2
+plot1 <- ggplot(data = C1, aes(x = time, y = conc1))+ geom_line() + theme_base() +
+   xlab("Time (minutes)") + ylab("Concentration (microMolar)") + ggtitle("Concentration vs Time", subtitle = "Compartment 1") + scale_y_log10()
+plot2 <- ggplot(data = C1, aes(x = time, y = conc2))+ geom_line() + theme_base() +
+   xlab("Time (minutes)") + ylab("Concentration (microMolar)") + ggtitle("Concentration vs Time", subtitle = "Compartment 2") + scale_y_log10()
+plot3 <- ggplot(data = C1, aes(x = time, y = conc3))+ geom_line() + theme_base() +
+   xlab("Time (minutes)") + ylab("Concentration (microMolar)") + ggtitle("Concentration vs Time", subtitle = "Compartment 3") + scale_y_log10()
